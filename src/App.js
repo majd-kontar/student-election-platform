92c8a953652a415d3a5eea239f9f20c33708a9a7
Majd Kontar <Majdkontar@gmail.com>
2022-10-16 16:24:41 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Login from "./components/Login";
+
+
+const App = (props) => {
+    return (
+        <Login/>
+    )
+}
+
+export default App
