e941b0f727061728e63ff503f73a9b7325f74eb3
Majd Kontar <Majdkontar@gmail.com>
2022-12-16 14:22:20 +0200

    ADD: LAU Logo

---
+++ src/components/OptionGeneratorUsingIndex.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from "react";
+
+const OptionGenerator = (props) => {
+    let opts = props.options
+    return (
+        <Fragment>
+            <option value="">Choose an option</option>
+            {opts.map((option) => (
+                <option value={opts.indexOf(option)}>
+                    {option}
+                </option>
+            ))}
+        </Fragment>)
+        ;
+}
+export default OptionGenerator;
